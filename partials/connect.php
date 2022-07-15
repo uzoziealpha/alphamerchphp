ec9f1a9b1fdcaa2027f4109b56a53a1cc2889ed7
Obi b. Uzozie <uzozie.obi@icloud.com>
2022-07-16 06:02:27 +0800

    Add existing project files to Git

---
+++ partials/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$host="localhost:3311";
+$user="root";
+$password="";
+$dbname="alphamerchstore";
+
+$connect=mysqli_connect($host,$user,$password,$dbname) or die("Unable to connect");
+
+?>
